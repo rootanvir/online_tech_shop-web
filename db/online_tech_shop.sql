ed5ba87850f8246529b05a301fcea786b3c8b403
Abdullah Shishir <abdullahshishir786@gmail.com>
2025-01-17 15:13:37 +0600

    db updated

--- db/online_tech_shop.sql
+++ db/online_tech_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2025 at 04:20 PM
+-- Generation Time: Jan 17, 2025 at 10:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -61,7 +61,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`employee_id`, `employee_password`, `employee_mobile_number`, `employee_email`, `employee_name`, `employee_gender`, `employee_dob`, `employee_address`, `employee_role`, `employee_joining_date`, `employee_salary`) VALUES
-('11111', 'password', '01712345678', 'admin@gamil.com', 'Mr Admin', 'Male', '01/01/1990', 'Bashundara R/A ,Dhaka ,Bangladesh', 'CEO', '01/01/2025', 80000);
+('11111', 'password', '01712345678', 'admin@gamil.com', 'Mr Admin', 'Male', '01/01/1990', 'Bashundara R/A ,Dhaka ,Bangladesh', 'CEO', '01/01/2025', 80000),
+('2200', '0011', '012', 'dw@gmail.com', 'ds', 'Male', '12/12/2014', 'Dhaka', 'Manager', '01/01/2025', 500),
+('2202', '0011', '012', 'dw@gmail.com', 'ds', 'Male', '12/12/2014', 'Dhaka', 'Admin', '01/01/2025', 500);
 
 -- --------------------------------------------------------
 
@@ -76,6 +78,22 @@ CREATE TABLE `product` (
   `product_price` double(10,2) DEFAULT NULL,
   `product_quantity` int(10) DEFAULT NULL,
   `product_location` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sells`
+--
+
+CREATE TABLE `sells` (
+  `sell_id` int(15) NOT NULL,
+  `customer_name` varchar(25) NOT NULL,
+  `customer_email` varchar(30) NOT NULL,
+  `products` varchar(70) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `price` float NOT NULL,
+  `time` datetime(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -99,6 +117,22 @@ ALTER TABLE `employee`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `sells`
+--
+ALTER TABLE `sells`
+  ADD PRIMARY KEY (`sell_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sells`
+--
+ALTER TABLE `sells`
+  MODIFY `sell_id` int(15) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
